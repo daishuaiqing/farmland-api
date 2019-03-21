0b3b371cec8d3e80e8be7f390a186bd9c620c873
daishuaiqing <daishuaiqing@163.com>
2019-03-21 18:33:16 +0800

    Initial commit

---
+++ src/main/java/com/daishuaiqing/farmland/FarmlandApplication.java
@@ -0,0 +1,13 @@
+package com.daishuaiqing.farmland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmlandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmlandApplication.class, args);
+    }
+
+}
